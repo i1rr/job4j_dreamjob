354d29a1971896ce55203c5bba55c046d8e9e12f
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2022-02-05 18:44:58 +1100

    sql fix

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE IF NOT EXISTS post (
-                      id SERIAL PRIMARY KEY,
-                      name TEXT
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );
 
 CREATE TABLE IF NOT EXISTS candidate (
-                           id SERIAL PRIMARY KEY,
-                           name TEXT
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );
 
 CREATE TABLE IF NOT EXISTS users (
@@ -14,4 +14,4 @@ CREATE TABLE IF NOT EXISTS users (
     name TEXT,
     email TEXT unique,
     password text
-)
+);
